6434affa0af63c8a48b0ef5c06b0d47240181e09
Mikhail Mefodyev <hg-4ybak@mail.ru>
2025-08-08 02:58:10 +0300

    feat: add docker image

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"log"
+
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 	"github.com/pkg/errors"
@@ -30,13 +32,13 @@ type (
 
 func New() (*Config, error) {
 	if err := godotenv.Load(); err != nil {
-		return nil, errors.Wrap(err, "load env file")
+		log.Print("warning: no .env file, skipping loading")
 	}
 
 	cfg := Config{}
 
 	if err := env.Parse(&cfg); err != nil {
-		return nil, errors.Wrap(err, "parse env file")
+		return nil, errors.Wrap(err, "parse env")
 	}
 
 	return &cfg, nil
